5428d3ed58c102f5e3742b7e50151f8e516c3b3c
Mohamed Iburan <mdiburan@gmail.com>
2024-03-15 06:54:42 +0530

    add vite plugin to generate dts files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
